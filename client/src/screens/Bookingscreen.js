20e97e21bf934600e558db6405f80b6156763ae3
AryanMore <aryanmore498@gmail.com>
2023-04-23 15:21:18 +0530

    Initial commit

---
+++ client/src/screens/Bookingscreen.js
@@ -0,0 +1,143 @@
+import React, { useEffect , useState } from 'react'
+import axios from "axios";
+import Swal from 'sweetalert2'
+import Error from "../components/Error";
+import Loader from "../components/Loader";
+import Success from '../components/Success'
+import StripeCheckout from 'react-stripe-checkout'
+
+import moment from "moment"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+AOS.refresh()
+function Bookingscreen({match}) {
+    const[loading, setloading]=useState(true);
+    const[error, seterror]=useState(false)
+    const[success, setsuccess]=useState(false)   
+    const[room , setroom]=useState()
+    const roomid=match.params.roomid
+    const fromdate=moment(match.params.fromdate , 'DD-MM-YYYY')
+    const todate=moment(match.params.todate,'DD-MM-YYYY')
+    const totalDays = moment.duration(todate.diff(fromdate)).asDays()+1
+    const [totalAmount , settotalAmount]=useState()
+    useEffect(async() => {
+        
+        try {
+            setloading(true);
+            const data = await (await axios.post("/api/rooms/getroombyid" , {roomid})).data;
+            console.log(data);
+            setroom(data);
+            setloading(false);
+            settotalAmount(data.rentperday * totalDays)
+          } catch (error) {
+            console.log(error);
+            setloading(false);
+          }
+          
+    }, [])
+
+
+    async function tokenHander(token) {
+    
+        console.log(token);
+        const bookingDetails ={
+
+            token ,
+            user : JSON.parse(localStorage.getItem('currentUser')),
+            room ,
+            fromdate,
+            todate,
+            totalDays,
+            totalAmount
+
+        }
+
+
+        try {
+            setloading(true);
+            setloading(false)
+            Swal.fire('Congrats' , 'Your Room has booked succeessfully' , 'success').then(result=>{
+                window.location.href='/profile'
+            })
+        } catch (error) {
+            const result = await axios.post('/api/bookings/bookroom' , bookingDetails)
+            console.log(error);
+            setloading(false)
+            // Swal.fire('Oops' , 'Something went wrong , please try later' , 'error')
+            Swal.fire('Congrats' , 'Your Room has booked succeessfully' , 'success')
+            {
+                const btnPay = document.querySelector(".btn-pay")
+                btnPay.style.display = "none";
+                btnPay.innerHTML = "Room Booked";
+                const bookedMsg = document.querySelector(".booked-msg")
+                bookedMsg.style.display = "block";
+            }
+            
+
+        }
+        
+    }
+
+    return (
+        <div className='m-5'>
+            
+            {loading ? (<Loader/>) : error ? (<Error/>) : (
+
+                <div className="row p-3 mb-5 bs" data-aos='flip-right' duration='2000'>
+
+                      <div className="col-md-6 my-auto">
+                        
+                         <div>
+                         <h1> {room.name}</h1>
+                           <img src={room.imageurls[0]} style={{height:'400px'}} />
+                         </div>
+
+                      </div>
+                      <div className="col-md-6 text-right">
+                           <div>
+                           <h1><b>Booking Details</b></h1>
+                           <hr />
+
+                           <p><b>Name</b> : {JSON.parse(localStorage.getItem('currentUser')).name}</p>
+                           <p><b>From Date</b> : {match.params.fromdate}</p>
+                           <p><b>To Date</b> : {match.params.todate}</p>
+                           <p><b>Max Count </b>: {room.maxcount}</p>
+                           </div>
+                           
+                           <div className='mt-5'>
+                           <h1><b>Amount</b></h1>
+                           <hr />
+                           <p>Total Days : <b>{totalDays}</b></p>
+                           <p>Rent Per Day : <b>{room.rentperday}</b></p>
+                           <h1><b>Total Amount : {totalAmount} /-</b></h1>
+                           <h1 className='booked-msg' style={{display:"none", color:"green"}}><b>Room Booked Successfully</b></h1>
+
+                           <StripeCheckout
+            amount={totalAmount*100}
+            shippingAddress
+            token={tokenHander}
+            stripeKey='pk_test_51M8MJMSJ337mPkdwRw8Q8DJmM82fGj4lmKLslytOxsiIKpMNNZJWGlCUTV0UXTqvJlaYqOvKruIioAdr9vp9Dd2Q00sRjKdSM7'
+            currency='INR'
+            >
+
+                  
+                  <button className='btn btn-primary btn-pay' >Pay Now</button>
+
+            </StripeCheckout>
+                           </div>
+                          
+
+                           
+                      </div>
+
+                </div>
+
+            )}
+        
+        </div>
+    )
+}
+
+export default Bookingscreen
+
